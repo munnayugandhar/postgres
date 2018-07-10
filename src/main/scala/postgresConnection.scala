6b0d4127e10244ad347374d88310b0b8a63e6721
yugandhar <yugandhar.motukuri@gmail.com>
2018-07-10 17:09:07 +0530

    changed table

--- src/main/scala/postgresConnection.scala
+++ src/main/scala/postgresConnection.scala
@@ -7,12 +7,12 @@ object postgresConnection extends App {
   val con_st = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres"
   val conn = DriverManager.getConnection(con_st)
   try {
+
     val stm = conn.createStatement
 
-    val rs = stm.executeQuery("SELECT custID from first_table")
-
+    val rs = stm.executeQuery("SELECT USER_EXPRESSION_QUERY from unmatched_feb")
     while(rs.next) {
-      println(rs.getString("custID"))
+      println(rs.getString("USER_EXPRESSION_QUERY"))
     }
   } finally {
     conn.close()
