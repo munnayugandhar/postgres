d4556e44eb47f197880c54702d9cc4deb19fe5cd
yugandhar <yugandhar.motukuri@gmail.com>
2018-07-19 16:41:29 +0530

    DI

--- src/main/scala/ProducerExample.scala
+++ src/main/scala/ProducerExample.scala
@@ -1,26 +1,22 @@
 import java.util.Properties
-
 import org.apache.kafka.clients.producer._
-import dataRetrieveFromPostgres.rs
 
-object ProducerExample extends App {
+class ProducerExample extends App {
 
-  val props = new Properties()
-  props.put("bootstrap.servers", "localhost:9092")
-  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-  props.put("zk.connect", "localhost:2181")
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("zk.connect", "localhost:2181")
+    val connn = new postgresConnection
 
-  val producer = new KafkaProducer[String, String](props)
-  val Topic: String = "Kafkatopic"
+    val producer = new KafkaProducer[String, String](props)
+    val Topic: String = "Kafkatopic1"
 
 
-
-  while(rs.next())
-  {
-      val record = new ProducerRecord[String, String]("Kafkatopic", rs.getString("USER_EXPRESSION_QUERY"))
+    while (connn.rs.next()) {
+      val record = new ProducerRecord[String, String]("Kafkatopic1", connn.rs.getString("USER_EXPRESSION_QUERY"))
       producer.send(record)
+    }
+
   }
-
-}
-
--- src/main/scala/postgresConnection.scala
+++ src/main/scala/postgresConnection.scala
@@ -1,12 +1,10 @@
 import java.sql.DriverManager
 
-object postgresConnection extends App {
+class postgresConnection extends App {
 
-  println("Postgres connector")
-
-  classOf[org.postgresql.Driver]
-  val con_st = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres"
-  val conn = DriverManager.getConnection(con_st)
-  val stm = conn.createStatement
-
+    classOf[org.postgresql.Driver]
+    val con_st = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres"
+    val conn = DriverManager.getConnection(con_st)
+    val stm = conn.createStatement
+    val rs = stm.executeQuery("SELECT USER_EXPRESSION_QUERY from unmatched_feb")
 }
