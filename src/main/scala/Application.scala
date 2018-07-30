13c03911f8f502bae53ffee27fb200036a815ce6
yugandhar <yugandhar.motukuri@gmail.com>
2018-07-30 16:54:59 +0530

    Defined Props Once

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,7 +1,5 @@
 import java.sql.{Connection, DriverManager}
 import java.util.Properties
-
-
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.postgresql.Driver
@@ -13,24 +11,18 @@ class Application extends App {
     val con_st = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres"
     DriverManager.getConnection(con_st)
   }
-
+  val prop = new Properties()
+    prop.put("bootstrap.servers", "localhost:9092")
+    prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    prop.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    prop.put("zk.connect", "localhost:2181")
   val expressionRepo = new UserExpressionRepository(getDbConnection())
 
   def getKafkaProducer = () => {
-    val props = new Properties()
-    props.put("bootstrap.servers", "localhost:9092")
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("zk.connect", "localhost:2181")
-    new KafkaProducer[String, String](props)
+    new KafkaProducer[String, String](prop)
   }
   def getKafkaConsumer = () => {
-    val props = new Properties()
-    props.put("bootstrap.servers", "localhost:9092")
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("zk.connect", "localhost:2181")
-    new KafkaConsumer[String, String](props)
+    new KafkaConsumer[String, String](prop)
   }
   val producer = new MessagePublisher(getKafkaProducer(), "Kafkatopic")
 
