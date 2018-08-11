81bb73b3634f1cb65b473e64596132ab167ce3b6
Sai Sree <saisree.edara@philips.com>
2018-08-11 11:58:49 +0200

    Know the flow

--- src/main/scala/MessagePublisher.scala
+++ src/main/scala/MessagePublisher.scala
@@ -4,11 +4,16 @@ class MessagePublisher(producer: Producer[String, String], topic: String)  {
     def sendMessage(messages: List[String])  {
         messages.foreach(m => {
           val record = new ProducerRecord[String, String](topic,m)
+          print("Before Send")
           producer.send(record, (_: RecordMetadata, exception: Exception) => {
+            print("Sleeping for 10 sec")
+            Thread.sleep(10000)
             if (exception != null) {
               throw new MessagePublishException()
             }
+            print("Woke up")
           })
         })
+      print("After Send")
     }
 }
